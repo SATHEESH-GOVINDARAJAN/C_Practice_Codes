5695c198f96735414f2e37923060011da8473008
SATHEESH GOVINDARAJAN <personal.satheesh.govindarajan17@gmail.com>
2026-05-09 18:07:13 +0530

    Update: Renamed 'n' to 'user_count' and refine input prompt for clarity

--- n_addition.c
+++ n_addition.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int n, i, current_number;
+    int user_count, i, current_number;
     int total_sum = 0;
 
-    printf("How many numbers do you want to add? ");
-    scanf("%d", &n);
+    printf("Enter how many numbers to be added: ");
+    scanf("%d", &user_count);
 
-    for(i = 1; i <= n; i++) {
+    for(i = 1; i <= user_count; i++) {
         printf("Enter number %d: ", i);
         scanf("%d", &current_number);
         total_sum = total_sum + current_number;
     }
 
-    printf("\nSum of the %d numbers is: %d\n", n, total_sum);
+    printf("\nSum of the %d numbers is: %d\n", user_count, total_sum);
 
     return 0;
 }
